bea21a887bd6d807073e74ebcf74f70d7a067e93
Jacob Castanon <jacastanon01@gmail.com>
2024-03-24 23:40:03 -0500

    defined copy_contents function to recursively copy the static directory into a public folder to host the files

--- src/main.py
+++ src/main.py
@@ -1,15 +1,41 @@
 import sys
-
-from .textnode import TextNode
-from .htmlnode import HTMLNode
-import src.block_markdown as bm
+import os
+import shutil
 
 
 def main():
-    with open("src/test.md", "r") as f:
-        bm.markdown_to_html_node(f.read())
+    abs_path = os.getcwd()
 
-    print("HELLO WORLD")
+    if os.path.exists(f"{abs_path}/public"):
+        shutil.rmtree(f"{abs_path}/public")
+
+    sourcedir = os.path.join(abs_path, "static")
+    if not os.path.exists(sourcedir):
+        raise NotADirectoryError("static directory does not exist in src")
+    destination = os.path.join(abs_path, "public")
+    listdirs = os.listdir(sourcedir)
+    copy_contents(sourcedir, destination)
+
+
+def copy_contents(source: str, destination: str):
+    listdirs = os.listdir(
+        source
+    )  # list all items in the source directory for processing
+
+    for item in listdirs:
+        # join paths for static and public folders
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+
+        if os.path.isfile(source_path):
+            # copy static file to public directory
+            shutil.copyfile(source_path, destination_path)
+        else:
+            if not os.path.exists(destination_path):
+                # create directory
+                os.makedirs(destination_path)
+            # recursively copy nested directories and their contents
+            copy_contents(source_path, destination_path)
 
 
 if __name__ == "__main__":
